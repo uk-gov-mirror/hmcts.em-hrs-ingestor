0ea59febaf26f05adf63d71e1b290cb2cbf3d25c
Semenu Aggor <semenu.aggor@hmcts.net>
2021-04-21 07:43:07 +0100

    EM-3615 Add service authorisation header to requests going to em-hrs-api

--- src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/config/OkHttpClientConfig.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/config/OkHttpClientConfig.java
@@ -1,19 +1,31 @@
 package uk.gov.hmcts.reform.em.hrs.ingestor.config;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 @Configuration
 public class OkHttpClientConfig {
 
     @Bean
-    public OkHttpClient provideOkHttpClient() {
+    public OkHttpClient provideOkHttpClient(final AuthTokenGenerator authTokenGenerator) {
         final HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor()
             .setLevel(HttpLoggingInterceptor.Level.BASIC);
 
         return new OkHttpClient.Builder()
+            .addInterceptor(chain -> {
+                final Request original = chain.request();
+
+                final Request request = original.newBuilder()
+                    .header("ServiceAuthorization", authTokenGenerator.generate())
+                    .method(original.method(), original.body())
+                    .build();
+
+                return chain.proceed(request);
+            })
             .addInterceptor(loggingInterceptor)
             .build();
     }
