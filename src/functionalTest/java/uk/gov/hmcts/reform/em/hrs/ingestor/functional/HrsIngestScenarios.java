e4f3ed2a037c5b7bb9db7fd1644965c68150b763
rajkatla-hmcts <72351979+rajkatla-hmcts@users.noreply.github.com>
2021-04-29 15:16:11 +0100

    clean-up and generate serenity reports

--- src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/ingestor/functional/HrsIngestScenarios.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/ingestor/functional/HrsIngestScenarios.java
@@ -4,12 +4,10 @@ import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.WithTag;
 import net.thucydides.core.annotations.WithTags;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +16,6 @@ import uk.gov.hmcts.reform.em.hrs.ingestor.functional.config.AzureClient;
 import uk.gov.hmcts.reform.em.hrs.ingestor.functional.util.TestUtil;
 
 import java.time.Duration;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
@@ -27,16 +24,12 @@ import static org.awaitility.Awaitility.await;
 @TestPropertySource("classpath:application.yml")
 @RunWith(SpringIntegrationSerenityRunner.class)
 @WithTags({@WithTag("testType:Functional")})
-public class HrsIngestScenarioTest {
+public class HrsIngestScenarios {
 
     private static final String HRS_BLOBSTORE_FOLDER1 = "audiostream999997";
     private static final String HRS_BLOBSTORE_FOLDER2 = "audiostream999998";
     private static final String HRS_BLOBSTORE_FOLDER3 = "audiostream999999";
-    private static final Duration TEN_SECONDS = Duration.ofSeconds(10);
     private static final Duration THIRTY_SECONDS = Duration.ofSeconds(30);
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(HrsIngestScenarioTest.class);
-
 
     @Value("${test.url}")
     private String testUrl;
@@ -44,72 +37,46 @@ public class HrsIngestScenarioTest {
     @Autowired
     private TestUtil testUtil;
 
-    @BeforeEach
+    @Before
     public void setup() {
         testUtil.clearHrsContainer();
     }
 
-    @AfterEach
+    @After
     public void clear() {
         testUtil.clearHrsContainer();
     }
 
     @Test
     public void shouldIngestFilesFromCvpBlobStoreToHrsBlobStore() {
-        Set<String> currentContents = testUtil.getHrsBlobsFrom("/");
-
-        LOGGER.warn("Current Contents of HRS Folder: {}",String.join(",", currentContents));
-
-        LOGGER.warn("Waiting for folder to be empty");
-        await()
-            .atMost(THIRTY_SECONDS)
-            .untilAsserted(() -> assertThat(testUtil.getHrsBlobsFrom(HRS_BLOBSTORE_FOLDER1))
-                .size()
-                .isEqualTo(0));
-
-
-        currentContents = testUtil.getHrsBlobsFrom("/");
-        LOGGER.warn("Current Contents of HRS Folder: {}",String.join(",", currentContents));
-
-
-        LOGGER.warn("Ingesting");
         SerenityRest
             .given()
             .relaxedHTTPSValidation()
             .baseUri(testUrl)
             .get("/ingest")
-            .then()
+            .then().log().all()
             .assertThat()
             .statusCode(200);
 
 
-        LOGGER.warn("Waiting 10 seconds");
-        try {
-            Thread.sleep(10000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        currentContents = testUtil.getHrsBlobsFrom("/");
-        LOGGER.warn("Current Contents of HRS Folder: {}",String.join(",", currentContents));
-
-
-        LOGGER.warn("Checking for folder 1 contents");
         await()
             .atMost(THIRTY_SECONDS)
             .untilAsserted(() -> assertThat(testUtil.getHrsBlobsFrom(HRS_BLOBSTORE_FOLDER1))
+                .containsAll(testUtil.getFilesForFolder(HRS_BLOBSTORE_FOLDER1))
                 .size()
                 .isEqualTo(5));
 
         await()
             .atMost(THIRTY_SECONDS)
             .untilAsserted(() -> assertThat(testUtil.getHrsBlobsFrom(HRS_BLOBSTORE_FOLDER2))
+                .containsAll(testUtil.getFilesForFolder(HRS_BLOBSTORE_FOLDER2))
                 .size()
                 .isEqualTo(3));
 
         await()
             .atMost(THIRTY_SECONDS)
             .untilAsserted(() -> assertThat(testUtil.getHrsBlobsFrom(HRS_BLOBSTORE_FOLDER3))
+                .containsAll(testUtil.getFilesForFolder(HRS_BLOBSTORE_FOLDER3))
                 .size()
                 .isEqualTo(1));
     }
