bd2ff0eb6cd7b54a136e1418d98dce3d2748582c
Paul Crimes <paul.crimes@solirius.com>
2021-11-04 14:25:34 +0000

    remove unused import

--- src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/config/AzureStorageConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/config/AzureStorageConfiguration.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Optional;
-
 @Configuration
 public class AzureStorageConfiguration {
 
