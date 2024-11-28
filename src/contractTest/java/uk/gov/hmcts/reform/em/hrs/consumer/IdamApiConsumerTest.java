0fb9e2863d74ac457443c590a32061f66ac3e610
MichaelJacob21012 <88201349+MichaelJacob21012@users.noreply.github.com>
2024-11-28 15:51:00 +0100

    Em 6331 update pact (#1155)
    * update pact to 4.6.15
    
    * enable pact in preview
    
    * Revert "enable pact in preview"
    
    This reverts commit 05879c89dad1f7c37296db80ceb32302353d4cd3.

--- src/contractTest/java/uk/gov/hmcts/reform/em/hrs/consumer/IdamApiConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/hrs/consumer/IdamApiConsumerTest.java
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.reform.em.hrs.consumer;
 
 import au.com.dius.pact.consumer.MockServer;
-import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.dsl.DslPart;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
-import au.com.dius.pact.model.RequestResponsePact;
-import com.google.common.collect.Maps;
+import au.com.dius.pact.core.model.PactSpecVersion;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.json.JSONArray;
@@ -22,7 +22,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import wiremock.com.google.common.collect.Maps;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -31,6 +33,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("contract")
 @ExtendWith(PactConsumerTestExt.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@PactTestFor(pactVersion = PactSpecVersion.V3)
 public class IdamApiConsumerTest {
 
     private static final String IDAM_OPENID_TOKEN_URL = "/o/token";
@@ -38,8 +41,6 @@ public class IdamApiConsumerTest {
 
     @Pact(provider = "Idam_api", consumer = "hrs_ingestor_api")
     public RequestResponsePact executeGetIdamAccessTokenAndGet200(PactDslWithProvider builder) throws JSONException {
-        String[] rolesArray = new String[1];
-        rolesArray[0] = "citizen";
         Map<String, String> requestheaders = Maps.newHashMap();
         requestheaders.put("Content-Type", "application/x-www-form-urlencoded");
 
@@ -51,7 +52,9 @@ public class IdamApiConsumerTest {
         params.put("password", "Password123");
         params.put("forename", "emCaseOfficer");
         params.put("surname", "jar123");
-        params.put("roles", rolesArray);
+
+        List<String> rolesList = List.of("citizen");
+        params.put("roles", rolesList);
 
         return builder
             .given("a user exists", params)
