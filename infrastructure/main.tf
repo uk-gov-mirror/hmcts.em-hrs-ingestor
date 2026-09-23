2dd714d231bd12780755a5e0b3819e10e021c198
mdayican <mdayican@gmail.com>
2026-09-23 16:15:24 +0100

    EM-7365 point master branch for key_vault and postgresql terraform modules (#1615)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "key-vault" {
-  source                       = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  source                       = "git@github.com:hmcts/cnp-module-key-vault"
   product                      = local.app_full_name
   env                          = var.env
   tenant_id                    = var.tenant_id
